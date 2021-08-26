e9c3738130db087aab10c3cc2498c34351802963
dineshssdn-867 <66898317+dineshssdn-867@users.noreply.github.com>
2021-08-26 13:18:32 +0530

    Update urls.py

--- user_control/urls.py
+++ user_control/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('login', LoginView.as_view()),
     path('register', RegisterView.as_view()),
     path('refresh', RefreshView.as_view()),
+    path('me', MeView.as_view()),
+    path('logout', LogoutView.as_view()),
 ] 
